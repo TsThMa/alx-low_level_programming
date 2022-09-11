6fffb74ad7ebf21140e9a1395edc6f617d296edf
Your Name <you@example.com>
2022-09-11 02:43:49 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/*
+/**
 * main - Entry point
 *
-*Return: Alwas 0 (Success)
+* Return: Alwas 0 (Success)
 */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle"\n)
-return (0);
+ puts("Programming is like building a multilingual puzzle")\n;
+    return (0);
 }
