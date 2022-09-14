6cbe3c918c7a281814f121f60d595465085c13b9
Your Name <you@example.com>
2022-09-14 12:39:07 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 
-/**
-*main - Entry point
-*
-*Return: Always 0 (Success)
-*/
+
+
 int main(void)
-{	
-char ch = '_putchar';
-putchar(ch);
-return (0);
+
+{
+
+		char word[8] = "_putchar";
+
+			int i;
+
+
+
+				for (i = 0; i < 8; i++)
+
+						putchar(word[i]);
+
+					putchar('\n');
+
+						
+
+
+
+						return (0);
+
 }
+
+
