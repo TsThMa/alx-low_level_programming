0094a29133670c4102b17e36d8fcb6fc80eaef12
Your Name <you@example.com>
2022-09-14 13:56:37 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,20 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * main - check the code
+ * main - Entry point 
  *
- * Return: Always (0).
+ * Return: 0 (Success)
  */
 int main(void)
 {
+	for(int x = 'A'; x <= 'Z'; x++)
 
-for (int x = 'A'; x <= 'Z'; x++)
-{
-	int lower_x = tolower(x);
+	{
 
-putchar(lower_x);
-}
+		    int lower_x = tolower(x);
+
+		        putchar(lower_x);
+
+	}
 return (0);
 }
