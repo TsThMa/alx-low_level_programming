ee9d15e7192ffdd5846de94adbb59499a43ed494
Your Name <you@example.com>
2022-09-14 13:28:10 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+
+/**
+
+ * main - check the code
+
+ *
+
+ * Return: Always 0.
+
+ */
+
+int main(void)
+{
+
+    for(int x = 'A'; x <= 'Z'; x++)
+
+        putchar(x);
+
+    return 0;
+
+}
