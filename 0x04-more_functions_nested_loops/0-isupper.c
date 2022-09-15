702f25c52c38384baf497808b4e1f9f133f17978
Your Name <you@example.com>
2022-09-15 01:53:45 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h"
+
+/**
+ *main - check the code.
+ *
+ *return 1 if uppercase, return 0 otherwise
+ */
+int main(void)
+{
+	    
+	    char c;
+	        c = 'A';
+		    
+		    if (c >= 65 && c <= 90)
+
+			        return (1);
+		        else return (0);
+}
+
