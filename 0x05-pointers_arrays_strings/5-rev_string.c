16b82283f8e0e4edc02a36bd94621b0d37ac75e0
Your Name <you@example.com>
2022-09-20 14:57:13 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - a function that reverses a string.
+ * @s: The string that will be reversed.
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+}
