aebd73fe1b026e5cec22f46855e08b25ab328d71
Your Name <you@example.com>
2022-09-22 17:33:55 -0700

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,8 +8,16 @@
  */
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122 &&
-		c >= 65 && c >= 90)
-			return (1);
-	return (0);
+	char lower, upper;
+	int isletter = 0;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		for (upper = 'A'; upper <= 'Z'; upper++)
+		{
+			if (c == lower || c == upper)
+				isletter = 1;
+		}
+	}
+	return (isletter);
 }
