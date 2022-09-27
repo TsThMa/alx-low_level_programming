6b6e895e66599fb029aa89f504ac96a8195ac670
Your Name <you@example.com>
2022-09-27 06:36:44 -0700

    Task 9

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - a function that locates a character in a string
+ * @s: string
+ * @c: a character
+ * Return: a poiter to c if found or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	char _strchr[]= "hello./0"
+		
