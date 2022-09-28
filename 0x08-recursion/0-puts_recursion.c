ef621687771e7f1188e94269fc3afba842735f5e
Your Name <you@example.com>
2022-09-28 16:41:52 -0700

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen -is a function that returns the length of a string.
+ * @s: is a string
+ * Return: nothing.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+	len++;
+	return (len);
+}
+
+/**
+ * _puts_recursion -  a function that prints a string, followed by a new line.
+ * @s: a pointer to string.
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	int len = 0;
+	char str = "Puts with recursion";
+
+	if (len >= 2)
+	len++
+		_puts_recursion(s
+	}
+	return;
+}
