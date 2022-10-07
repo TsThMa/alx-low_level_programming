0c0fcab82d5ea8c5b47e6d7c2b0e5c4ef4ab4295
Your Name <you@example.com>
2022-10-07 05:13:32 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,18 @@
 #include <unistd.h>
 #include <sys/syscall.h>
+#include <stdio.h>
 
 /**
- * a C program that prints a string exactl, followed by '\n'
+ * main - Entry point
+ * Write a program that prints a string exactl, followed by a new line.
  * Return: 1
  */
 int main(void)
 {
 
-const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-syscall(SYS_write, 1, msg, sizeof(msg)); 
+const char msg[] = "and that piece of art is useful\" - Dora Korpar, \
+2015-10-19";
+syscall(SYS_write, 1, msg, sizeof(msg));
+putchar('\n');
 return (1);
 }
