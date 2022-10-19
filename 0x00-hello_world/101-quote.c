586b3d0da09f3a9e79eaebab017b10753bd7e14c
Your Name <you@example.com>
2022-10-19 04:07:29 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,16 +3,27 @@
 #include <stdio.h>
 
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return:On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
  * main - Entry point
  * Write a program that prints a string exactl, followed by a new line.
  * Return: 1
  */
 int main(void)
 {
-
 const char msg[] = "and that piece of art is useful\" - Dora Korpar, \
 2015-10-19";
 syscall(SYS_write, 1, msg, sizeof(msg));
-putchar('\n');
+_putchar('\n');
 return (1);
 }
